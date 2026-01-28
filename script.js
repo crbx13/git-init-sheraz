ac02a4d0816dea3eea2866e94723ef0680fd84e8
crbx13 <sherazbessadok130@gmail.com>
2026-01-28 10:06:10 +0100

    feat: add additional element retrievals for file input, drop area, cover image, and navigation buttons in script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const audio = document.getElementById('audio');
 const PlayPauseBtn = document.getElementById("playPause");
 const progressBar = document.getElementById("progressBar");
 const fileInput = document.getElementById("fileInput");
+const dropArea = document.getElementById("dropArea");
+const coverImage = document.getElementById("coverImage");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
