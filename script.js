9adca0db7234ef0097d09ecf4fb7aed0532666a6
crbx13 <sherazbessadok130@gmail.com>
2026-02-04 16:38:32 +0100

    rewriting my script

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const audio = document
+const audio = document.getElementById("audio");
+const playPauseBtn = document.getElementById("playPlause");
+const progressBar = document.getElementById("progressBar");
